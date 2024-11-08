78adc004ddf82ff7100753a416cf72a81f10aeab
Sofia Horlacher <sofia.horlacher@hotmail.com>
2024-11-08 13:45:36 +0000

    change src path

--- src/main.py
+++ src/main.py
@@ -7,8 +7,8 @@ import os
 import wandb
 import torch
 import lightning as L
-from src.data_module import GLUEDataModule
-from src.model import GLUETransformer
+from data_module import GLUEDataModule
+from model import GLUETransformer
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint
 
